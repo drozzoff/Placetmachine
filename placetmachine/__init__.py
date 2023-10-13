913fd83724052d58352d182c30b6ad1f5caa1333
drozzoff <drozz.off@gmail.com>
2023-10-13 15:28:34 +0200

    Fix to the import order

--- placetmachine/__init__.py
+++ placetmachine/__init__.py
@@ -2,12 +2,11 @@ import os
 from placetmachine.placet.placetwrap import Placet
 from placetmachine.util import CoordTransformation
 from placetmachine.lattice.lattice import Beamline
-from placetmachine.machine import Machine
-
 
 __package_dir__ = os.path.dirname(__file__)
-
 __placet_files_dir__ = os.path.join(__package_dir__, 'placet_files')
+
+from placetmachine.machine import Machine
 
 __version__ = '0.0.1'
 
