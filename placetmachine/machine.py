9139c60da4ec5da47fd922f8463ffd6e060fd61d
drozzoff <drozz.off@gmail.com>
2024-08-14 16:21:54 +0200

    Updated `Machine` methods to support different knobs usage formats

--- placetmachine/machine.py
+++ placetmachine/machine.py
@@ -1058,7 +1058,7 @@ class Machine():
 		track_results.correction = "RF align"
 		return track_results
 
-	def apply_knob(self, knob: Knob, amplitude: float):
+	def apply_knob(self, knob: Knob, amplitude: float, strategy: str):
 		"""
 		Apply the knob and update the beamline offsets.
 
@@ -1073,10 +1073,13 @@ class Machine():
 			The knob to use.
 		amplitude
 			Amplitude to apply.
+		strategy
+			Strategy to use for calculations of the offsets when the knob has `step_size` 
+			defined. Default is 'simple_memory'.
 		"""
 		if knob not in self.beamline.attached_knobs:
 			raise ValueError("The knob provided does not exist!")
-		knob.apply(amplitude)
+		knob.apply(amplitude, strategy = strategy)
 
 	@verify_beam
 	def eval_track_results(self, beam: Beam, **extra_params) -> (pd.DataFrame, float, float):
@@ -1300,6 +1303,12 @@ class Machine():
 			When it is "natural", the elements' offsets associated with a `knob` are not reset.
 			Since `Knob.apply()` is additive function, an amplitude difference between the 
 			previous and current is applied.
+		knob_apply_strategy : str
+			Strategy to use for calculations of the offsets when knob has `step_size` defined. 
+			Default is 'simple_memory'. Possible options are:
+			```
+			["simple", "simple_memory", "min_scale", "min_scale_memory"]
+			```
 		fit : Callable
 			Function to fit the data.
 			**!!** Only works if the amound of observables is equaly **1**.
@@ -1338,6 +1347,9 @@ class Machine():
 		_iteration_types = ["natural", "with_cache"]
 		if isinstance(observables, str):
 			observables = [observables]
+		knob_apply_strategy = extra_params.get("knob_apply_strategy", "simple")
+		if not knob_apply_strategy in knob._strategies_available:
+			raise ValueError(f"Strategy '{knob_apply_strategy}' is not available. Possible options are {knob._strategies_available}.")
 
 		if not set(list(observables)).issubset(set(_obs_values)):
 			raise ValueError(f"The observables(s) '{observables}' are not supported")
@@ -1368,14 +1380,14 @@ class Machine():
 			
 			return table
 
-		def _eval_obs(knob: Knob, amplitude: float, iteration_type: str):
+		def _eval_obs(knob: Knob, amplitude: float, iteration_type: str, knob_apply_strategy: str):
 			"""
 			Maybe this function can be used universally, Machine wide.
 
 			It sets the knob, runs the track, reverts the changes, and returns the observable values.
 			*I use the similar function to test the knob performance.
 			"""
-			self.apply_knob(knob, amplitude)
+			self.apply_knob(knob, amplitude, knob_apply_strategy)
 			self._CACHE_LOCK['iterate_knob'] = True
 
 			obs = self.eval_obs(beam, observables, suppress_output = True)
@@ -1392,9 +1404,9 @@ class Machine():
 				amplitude_prev, obs = .0, None
 				for amplitude in knob_range:
 					if iteration_type == "with_cache":
-						obs = _eval_obs(knob, amplitude, iteration_type)
+						obs = _eval_obs(knob, amplitude, iteration_type, knob_apply_strategy)
 					elif iteration_type == "natural":
-						obs = _eval_obs(knob, amplitude - amplitude_prev, iteration_type)
+						obs = _eval_obs(knob, amplitude - amplitude_prev, iteration_type, knob_apply_strategy)
 						amplitude_prev = amplitude
 					else:
 						continue
@@ -1405,9 +1417,9 @@ class Machine():
 			amplitude_prev, obs = .0, None
 			for amplitude in knob_range:
 				if iteration_type == "with_cache":
-					obs = _eval_obs(knob, amplitude, iteration_type)
+					obs = _eval_obs(knob, amplitude, iteration_type, knob_apply_strategy)
 				elif iteration_type == "natural":
-					obs = _eval_obs(knob, amplitude - amplitude_prev, iteration_type)
+					obs = _eval_obs(knob, amplitude - amplitude_prev, iteration_type, knob_apply_strategy)
 					amplitude_prev = amplitude
 				else:
 					continue
